966891288cee8d1650dadf7f6fc981c1ceb84405
kabilanthiru07-creator <kabilanthiru07@gmail.com>
2026-02-06 11:36:22 +0530

    updated

--- admin-frontend/src/services/api.js
+++ admin-frontend/src/services/api.js
@@ -7,7 +7,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 // Request interceptor - Add JWT token to requests
