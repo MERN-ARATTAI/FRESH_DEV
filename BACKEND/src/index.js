d6b9ed43b2b4a877630194baa7c8d3b6513e9c9a
kabilanthiru07-creator <kabilanthiru07@gmail.com>
2026-02-06 11:33:16 +0530

    updated

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -18,12 +18,13 @@ import OrderRouter from './Routes/OrderRoute.js'
 const app = express()
 DatabaseConnection()
 
-app.use(cors({
-    credentials: true,
-    origin: ["http://localhost:5173", "http://localhost:5174"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+// app.use(cors({
+//     credentials: true,
+//     origin: ["http://localhost:5173", "http://localhost:5174","http://localhost:5175"],
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"]
+// }));
+app.use(cors())     
 app.use(express.json())
 app.use(cookieParser())
 
