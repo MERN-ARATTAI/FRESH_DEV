71448bf72e8e302534ad37deb9d4769a4e01e8a7
VijayKumar406 <milliontrillion276@gmail.com>
2026-02-09 22:49:45 +0530

    Updated

--- BACKEND/src/Utils/Mailer.js
+++ BACKEND/src/Utils/Mailer.js
@@ -9,7 +9,6 @@ const transport = nodemailer.createTransport({
     host:"smtp.gmail.com",
     port: 587,
     secure: false,
-    service: 'gmail',
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
